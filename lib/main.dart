58a372bfea6818c92b5b196d8e12ac2649848b9c
Parfait Guiri <parfait.guiri@gmail.com>
2025-11-23 20:21:30 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:griot_urban_app/constants/colors.dart';
 import 'package:griot_urban_app/screens/feed_screen.dart';
+import 'package:griot_urban_app/screens/login_screen.dart';
+import 'package:griot_urban_app/screens/signup_screen.dart';
+import 'package:griot_urban_app/screens/subscription_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: '.env'); // Charge les variables d'environnement
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
@@ -23,8 +28,50 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
         ),
       ),
-      home: const FeedScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const AuthChecker(),
+        '/login': (context) => const LoginScreen(),
+        '/signup': (context) => const SignupScreen(),
+        '/home': (context) => const FeedScreen(),
+        '/subscription': (context) => SubscriptionScreen(token: ModalRoute.of(context)!.settings.arguments as String),
+      },
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+// NOUVEAU : Vérifie l'authentification au démarrage
+class AuthChecker extends StatefulWidget {
+  const AuthChecker({super.key});
+
+  @override
+  State<AuthChecker> createState() => _AuthCheckerState();
+}
+
+class _AuthCheckerState extends State<AuthChecker> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAuth();
+  }
+
+  Future<void> _checkAuth() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('token');
+    if (token != null) {
+      Navigator.pushReplacementNamed(context, '/home');
+    } else {
+      Navigator.pushReplacementNamed(context, '/login');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
     );
   }
 }
