be12408ce7863c839c10bed38f1382de0bcc197d
Parfait Guiri <parfait.guiri@gmail.com>
2025-11-23 17:05:56 +0100

    Create colors.dart

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFFF6B35); // Orange masque
+  static const secondary = Color(0xFF4A90E2); // Bleu halo
+  static const background = Color(0xFFF5F5F5);
+  static const text = Color(0xFF333333);
+  static const white = Colors.white;
+}
